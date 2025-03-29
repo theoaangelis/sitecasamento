23088e61f919fa036f3161e7905676688b083343
Theo Araujo de Angelis <theoa.angelis@gmail.com>
2025-03-29 13:18:43 -0300

    altera

--- netlify/functions/fotos.js
+++ netlify/functions/fotos.js
@@ -12,14 +12,15 @@ const bucketName = process.env.MEUAPP_S3_BUCKET_NAME;
 exports.handler = async () => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type'
+    'Access-Control-Allow-Headers': 'Content-Type',
   };
 
   try {
     const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
-    const fotos = data.Contents.map(
-      (item) => `https://${bucketName}.s3.amazonaws.com/${item.Key}`
-    );
+    const fotos = data.Contents.map(item => ({
+      url: `https://${bucketName}.s3.amazonaws.com/${item.Key}`,
+      name: item.Key, // Nome correto do arquivo
+    }));
 
     return {
       statusCode: 200,
@@ -27,6 +28,7 @@ exports.handler = async () => {
       body: JSON.stringify(fotos),
     };
   } catch (err) {
+    console.error('Erro ao listar fotos:', err);
     return {
       statusCode: 500,
       headers,
